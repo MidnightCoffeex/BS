e7fe44aaaf1a5705dceea111759b2cd87c5ce5bb
MidnightCoffeex <maxim.alenik@study.hs-duesseldorf.de>
2023-10-11 00:05:57 +0200

    myshell Level 1

--- Course_2/myShell.c
+++ Course_2/myShell.c
@@ -1,18 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 
+void print_command_prompt(){
+  printf("myShell>");
 
-...
-while(1) {
-  print_command_promt();
-  read_and_save_command();
-
-  if(fork() > 0) {
-    /* parent process */
-    wait(status);
-  }
-  else {
-    /* child process */
-    execve(command, ...);
-  }
 }
-...
+
+
+char* read_and_save_command(){
+  char* command = (char*)malloc(256);
+  if(command == NULL){
+    perror("malloc");
+    exit(EXIT_FAILURE);
+  }
+  scanf("%255s", command);
+  return command;
+}
+
+int main(void){
+  while(1){
+    print_command_prompt();
+    char* command = read_and_save_command();
+
+  //Elternprozess
+    if (fork() > 0){
+      int status;
+      wait(&status);
+    }
+  //Kindprozess
+    else{
+      char *argv[] = {command, NULL};
+      char *envp[] = {NULL};
+      errno = 0;
+      if (execve(command, argv, envp) == -1){
+        perror("execve");
+        exit(EXIT_FAILURE);
+      }
+    }
+    free(command);
+  }
+  return 0;
+}
